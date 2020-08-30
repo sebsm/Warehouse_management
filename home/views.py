c473bd76864bee5bc8ad20e391febe632a9f00ec
selvert <sebastian.smolinski.stud@pw.edu.pl>
2020-08-30 14:42:39 +0200

    Useless files dumped

--- home/views.py
+++ home/views.py
@@ -15,6 +15,7 @@ from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, View
+from django.views.generic.base import TemplateView
 
 from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
 from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
